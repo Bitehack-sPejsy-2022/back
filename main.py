a533676ddde150e633be4842f622a6fde7c26225
kuba2kopec <kuba2kopec@gmail.com>
2022-01-15 16:27:13 +0100

    cors middleware

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from typing import List, Optional, Dict, Any
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from mockup_poi import generate_poi
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Poi(BaseModel):
     name: str
