7af6f12aa29b7c70537e30aaaea85d9b89ead054
Szymon Bednorz <dsonyyy@gmail.com>
2022-01-15 23:54:11 +0100

    images + cache

--- google_downloader.py
+++ google_downloader.py
@@ -1,4 +1,5 @@
 from io import BytesIO, TextIOWrapper
+import re
 from google_images_download import google_images_download
 import sys
 from unicodedata import normalize
@@ -7,6 +8,15 @@ from unicodedata import normalize
 def get_photos_from_google(city: str, key: str, num: int = 1):
     normalized = normalize('NFD', f'"{key}" {city}')
     key = normalized
+
+    try:
+        f = open("photos-cache", "r")
+        for line in f:
+            if line.split("\t")[0] == normalized:
+                return line.split("\t")[1]
+        f.close()
+    except:
+        pass
 
     old_stdout = sys.stdout
     sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
@@ -17,6 +27,7 @@ def get_photos_from_google(city: str, key: str, num: int = 1):
         "keywords": key,
         "limit": num,
         "print_urls": True,
+        "no_download": True
     }
     paths = response.download(arguments)
 
@@ -30,7 +41,14 @@ def get_photos_from_google(city: str, key: str, num: int = 1):
     for line in output.split("\n"):
         if line.startswith("Image URL:"):
             result.append(line.replace("Image URL: ", ""))
-    return result
+
+    if result:
+        f = open("photos-cache", "a")
+        f.write(normalized + "\t" + result[0] + "\n")
+        f.close()
+        return result[0]
+
+    return ""
 
 
 if __name__ == "__main__":
