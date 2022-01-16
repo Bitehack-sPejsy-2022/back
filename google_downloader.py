696d838b94f69d930942025f9af4a36eaa17290d
Szymon Bednorz <dsonyyy@gmail.com>
2022-01-16 03:10:14 +0100

    downloading photos

--- google_downloader.py
+++ google_downloader.py
@@ -3,14 +3,16 @@ import re
 import dryscrape
 from bs4 import BeautifulSoup
 
+session = dryscrape.Session()
 
 def get_photos_from_bing(city: str, key: str):
     key = f'"{key}" {city}'
 
-    key = key.replace("ó", "o").replace("ż", "z").replace("ź", "z").replace(
-        "ś", "s").replace("ć", "c").replace("ą", "a").replace("ę", "e").replace("ł", "l")
+    key = key.lower().replace("ó", "o").replace("ż", "z").replace("ź", "z").replace(
+        "ś", "s").replace("ć", "c").replace("ą", "a").replace("ę", "e").replace("ł", "l").replace("ń", "n")
 
-    session = dryscrape.Session()
+    print(key)
+
     session.visit(
         f'https://www.bing.com/images/search?q={key}&form=HDRSC2&first=1&tsc=ImageBasicHover')
     response = session.body()
