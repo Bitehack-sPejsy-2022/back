1a817ab5466a59fa8e3564eda1a8940430d6ef28
Szymon Bednorz <dsonyyy@gmail.com>
2022-01-16 03:26:59 +0100

    photos cache

--- google_downloader.py
+++ google_downloader.py
@@ -5,11 +5,24 @@ from bs4 import BeautifulSoup
 
 session = dryscrape.Session()
 
+
 def get_photos_from_bing(city: str, key: str):
     key = f'"{key}" {city}'
 
     key = key.lower().replace("ó", "o").replace("ż", "z").replace("ź", "z").replace(
         "ś", "s").replace("ć", "c").replace("ą", "a").replace("ę", "e").replace("ł", "l").replace("ń", "n")
+
+    try:
+        with open("cache-photos", "r") as f:
+            for line in f:
+                line = line.split("\t")
+                if line:
+                    if line[0] == key:
+                        return line[1].strip()
+            else:
+                pass
+    except:
+        pass
 
     print(key)
 
@@ -21,8 +34,14 @@ def get_photos_from_bing(city: str, key: str):
     for i in soup.find_all("a", {"class": "iusc"}):
         a = re.search('\"murl\":\"[^\"]+\"', str(i))
         if a:
-            return a.group(0)[8:-1]
+            with open("cache-photos", "a") as f:
+                f.write(key + "\t" + a.group(0)[8:-1] + "\n")
+            return a.group(0)[8:-1].strip()
             break
+
+    with open("cache-photos", "a") as f:
+        f.write(key + "\t" + " " + "\n")
+        return ""
     return ""
 
 
